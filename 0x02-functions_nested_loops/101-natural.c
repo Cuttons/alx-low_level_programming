2469ada8e311ed94e248c7e26795ec2ca888ecf9
Cuttons <wendynnaochie@gmail.com>
2023-08-27 04:24:58 -0700

    103-fibonacci

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,4 @@
-#include "main.h"	   
+#include <stdio.h>
 
 /**
 * main - check the code.
@@ -6,16 +6,26 @@
 * Return: Always 0.
 */
 
-
-int main(void)
-{
-print_times_table(3);
-_putchar('\n');
-print_times_table(5);
-_putchar('\n');
-print_times_table(98);
-_putchar('\n');
-print_times_table(12);
-return (0);
+void print_times_table(int n) {
+    for (int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
 }
 
+int main() {
+    print_times_table(3);
+}
+_putchar('\n');
+{
+print_times_table(5);
+}
+_putchar('\n');
+{
+print_times_table(98);
+}
+_putchar('\n');
+{
+print_times_table(12);
+}
+return (0);
+}
